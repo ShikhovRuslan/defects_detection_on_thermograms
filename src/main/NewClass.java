177175873d268ff65a87ad7bc8b1d984eb3a6732
R. Shikhov (Windows) <r.n.shikhov@gmail.com>
2020-08-16 20:29:56 +0700

    16.08.20 - 1

--- src/main/NewClass.java
+++ src/main/NewClass.java
@@ -16,39 +16,6 @@ https://github.com/grumlimited/geocalc
 
  */
 
-/**
- * Система c'x'y'z' пиксельных координат. Центр c' находится в нижней левой точке матрицы камеры. Ось c'x' направлена
- * вдоль нижней стороны матрицы, ось c'y' - вдоль левой боковой стороны матрицы, а ось c'z' - вверх.
- */
-class Pixel {
-    /**
-     * Номер пикселя по оси c'x'.
-     */
-    private final int i;
-    /**
-     * Номер пикселя по оси c'y'.
-     */
-    private final int j;
-
-    Pixel(int i, int j) {
-        this.i = i;
-        this.j = j;
-    }
-
-    int getI() {
-        return i;
-    }
-
-    int getJ() {
-        return j;
-    }
-
-    @Override
-    public String toString() {
-        return "Pixel{i=" + i + ", j=" + j + "}";
-    }
-}
-
 public class NewClass {
     /**
      * Разрешение матрицы по горизонтали.
@@ -98,9 +65,21 @@ public class NewClass {
      * Пиксельные координаты углов матрицы, начиная с верхнего левого угла и заканчивая нижним левым.
      */
     private enum Corners {
+        /**
+         * Верхний левый угол матрицы.
+         */
         C0(0, RES_Y - 1),
+        /**
+         * Верхний правый угол матрицы.
+         */
         C1(RES_X - 1, RES_Y - 1),
+        /**
+         * Нижний правый угол матрицы.
+         */
         C2(RES_X - 1, 0),
+        /**
+         * Нижний левый угол матрицы.
+         */
         C3(0, 0);
 
         private final int i;
@@ -157,14 +136,6 @@ public class NewClass {
         return Coordinate.fromDMS(degrees, minutes, seconds);
     }
 
-    /*
-    // https://www.igismap.com/formula-to-find-bearing-or-heading-angle-between-two-points-latitude-longitude/
-    private static double bearing(Point point, Point centre) {
-        double X = cos((PI / 180) * point.latitude) * sin((PI / 180) * (point.longitude - centre.longitude));
-        double Y = cos((PI / 180) * centre.latitude) * sin((PI / 180) * point.latitude) - sin((PI / 180) * centre.latitude) * cos((PI / 180) * point.latitude) * cos((PI / 180) * (point.longitude - centre.longitude));
-        return (180 / PI) * (atan2(X, Y) > 0 ? atan2(X, Y) : 2 * PI + atan2(X, Y));
-    }*/
-
     private static Pixel toPixel(Point point, Point centre, double yaw, double height) {
         double earthDistance = EarthCalc.harvesineDistance(point, centre);
         double omega = (PI / 180) * (360 - yaw - EarthCalc.bearing(centre, point));
@@ -172,110 +143,9 @@ public class NewClass {
         return new Pixel((int) round(pixelDistance * cos(omega)), (int) round(pixelDistance * sin(omega)));
     }
 
-    private static class Point2 {
-        double x, y;
-
-        public Point2(double newX, double newY) {
-            x = newX;
-            y = newY;
-        }
-    }
-
-    //https://vscode.ru/prog-lessons/nayti-tochku-peresecheniya-otrezkov.html
-    //метод, проверяющий пересекаются ли 2 отрезка [p1, p2] и [p3, p4]
-    private boolean checkIntersectionOfTwoLineSegments(Point2 p1, Point2 p2, Point2 p3, Point2 p4) {
-        //сначала расставим точки по порядку, т.е. чтобы было p1.x <= p2.x
-        if (p2.x < p1.x) {
-            Point2 tmp = p1;
-            p1 = p2;
-            p2 = tmp;
-        }
-        //и p3.x <= p4.x
-        if (p4.x < p3.x) {
-            Point2 tmp = p3;
-            p3 = p4;
-            p4 = tmp;
-        }
-
-        //проверим существование потенциального интервала для точки пересечения отрезков
-        if (p2.x < p3.x) {
-            return false; //ибо у отрезков нету взаимной абсциссы
-        }
-
-        //если оба отрезка вертикальные
-        if ((p1.x - p2.x == 0) && (p3.x - p4.x == 0)) {
-            //если они лежат на одном X
-            if (p1.x == p3.x)
-                //проверим пересекаются ли они, т.е. есть ли у них общий Y
-                //для этого возьмём отрицание от случая, когда они НЕ пересекаются
-                if (!((Math.max(p1.y, p2.y) < Math.min(p3.y, p4.y)) ||
-                        (Math.min(p1.y, p2.y) > Math.max(p3.y, p4.y))))
-                    return true;
-
-            return false;
-        }
-
-        //найдём коэффициенты уравнений, содержащих отрезки
-        //f1(x) = A1*x + b1 = y
-        //f2(x) = A2*x + b2 = y
-
-        //если первый отрезок вертикальный
-        if (p1.x - p2.x == 0) {
-
-            //найдём Xa, Ya - точки пересечения двух прямых
-            double Xa = p1.x;
-            double A2 = (p3.y - p4.y) / (p3.x - p4.x);
-            double b2 = p3.y - A2 * p3.x;
-            double Ya = A2 * Xa + b2;
-
-            if (p3.x <= Xa && p4.x >= Xa && Math.min(p1.y, p2.y) <= Ya &&
-                    Math.max(p1.y, p2.y) >= Ya) {
-
-                return true;
-            }
-
-            return false;
-        }
-
-        //если второй отрезок вертикальный
-        if (p3.x - p4.x == 0) {
-
-            //найдём Xa, Ya - точки пересечения двух прямых
-            double Xa = p3.x;
-            double A1 = (p1.y - p2.y) / (p1.x - p2.x);
-            double b1 = p1.y - A1 * p1.x;
-            double Ya = A1 * Xa + b1;
-
-            if (p1.x <= Xa && p2.x >= Xa && Math.min(p3.y, p4.y) <= Ya &&
-                    Math.max(p3.y, p4.y) >= Ya) {
-
-                return true;
-            }
-
-            return false;
-        }
-
-        //оба отрезка невертикальные
-        double A1 = (p1.y - p2.y) / (p1.x - p2.x);
-        double A2 = (p3.y - p4.y) / (p3.x - p4.x);
-        double b1 = p1.y - A1 * p1.x;
-        double b2 = p3.y - A2 * p3.x;
-
-        if (A1 == A2) {
-            return false; //отрезки параллельны
-        }
-
-        //Xa - абсцисса точки пересечения двух прямых
-        double Xa = (b2 - b1) / (A1 - A2);
-
-        if ((Xa < Math.max(p1.x, p3.x)) || (Xa > Math.min(p2.x, p4.x))) {
-            return false; //точка Xa находится вне пересечения проекций отрезков на ось X
-        } else {
-            return true;
-        }
-    }
-
     public static void main(String[] args) {
+
+        System.out.println(Pixel.findIntersection(new Pixel(0, 0), new Pixel(3, 3), new Pixel(3, 3), new Pixel(5, 5)));
 
         double s1 = earthDistance(new Pixel(0, 0), new Pixel(RES_X - 1, 0), 152);
         double s2 = earthDistance(new Pixel(0, 0), new Pixel(0, RES_Y - 1), 152);
---
+++ src/main/Pixel.java
@@ -0,0 +1,123 @@
+package main;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+/**
+ * Система c'x'y'z' пиксельных координат. Центр c' находится в нижней левой точке матрицы камеры. Ось c'x' направлена
+ * вдоль нижней стороны матрицы, ось c'y' - вдоль левой боковой стороны матрицы, а ось c'z' - вверх.
+ */
+public class Pixel {
+    /**
+     * Номер пикселя по оси c'x'.
+     */
+    private final int i;
+    /**
+     * Номер пикселя по оси c'y'.
+     */
+    private final int j;
+
+    Pixel(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    Pixel(double iD, double jD) {
+        this((int) Math.round(iD), (int) Math.round(jD));
+    }
+
+    int getI() {
+        return i;
+    }
+
+    int getJ() {
+        return j;
+    }
+
+    @Override
+    public String toString() {
+        return "Pixel{i=" + i + ", j=" + j + "}";
+    }
+
+    /**
+     * Возвращает точку пересечения отрезков [p1, p2] и [p3, p4] в случае, если они пересекаются по единственной точке и
+     * эта точка является внутренней для каждого из этих отрезков.
+     * <p>
+     * (https://vscode.ru/prog-lessons/nayti-tochku-peresecheniya-otrezkov.html)
+     *
+     * @return Точка пересечения отрезков или точка (-1,-1)
+     */
+    static Pixel findIntersection(Pixel p1, Pixel p2, Pixel p3, Pixel p4) {
+        Pixel no = new Pixel(-1, -1);
+
+        // Добиваемся, чтобы было p1.i <= p2.i и p3.i <= p4.i.
+        if (p2.getI() < p1.getI()) {
+            Pixel tmp = p1;
+            p1 = p2;
+            p2 = tmp;
+        }
+        if (p4.getI() < p3.getI()) {
+            Pixel tmp = p3;
+            p3 = p4;
+            p4 = tmp;
+        }
+
+        // У отрезков нет общей абсциссы или ординаты.
+        if ((p2.getI() < p3.getI() || p4.getI() < p1.getI()) ||
+                (max(p1.getJ(), p2.getJ()) < min(p3.getJ(), p4.getJ()) || max(p3.getJ(), p4.getJ()) < min(p1.getJ(), p2.getJ())))
+            return no;
+
+        // Отрезки либо оба вертикальные, либо оба горизонтальные.
+        if (p1.getI() == p2.getI() && p3.getI() == p4.getI() || p1.getJ() == p2.getJ() && p3.getJ() == p4.getJ()) {
+            return no;
+        }
+
+        // Если второй отрезок вертикальный, то его делаем первым.
+        if (p3.getI() == p4.getI()) {
+            Pixel tmp1 = p1;
+            Pixel tmp2 = p2;
+            p1 = p3;
+            p2 = p4;
+            p3 = tmp1;
+            p4 = tmp2;
+        }
+
+        // (x0, y0) - точка пересечения прямых, содержащих отрезки.
+        // Прямые, содержащие отрезки (в случае невертикальных отрезков):
+        // y = a1*x + b1,
+        // y = a2*x + b2.
+
+        double x0, y0, a1, a2, b1, b2;
+
+        // Первый отрезок вертикальный (а второй - невертикальный).
+        if (p1.getI() == p2.getI()) {
+            x0 = p1.getI();
+            a2 = (p3.getJ() - p4.getJ()) / (p3.getI() - p4.getI() + 0.);
+            b2 = p3.getJ() - a2 * p3.getI();
+            y0 = a2 * x0 + b2;
+
+            if (p3.getI() < x0 && x0 < p4.getI() &&
+                    Math.min(p1.getJ(), p2.getJ()) < y0 && y0 < Math.max(p1.getJ(), p2.getJ()))
+                return new Pixel(x0, y0);
+            return no;
+        }
+
+        // Оба отрезка невертикальные.
+        a1 = (p1.getJ() - p2.getJ()) / (p1.getI() - p2.getI() + 0.);
+        a2 = (p3.getJ() - p4.getJ()) / (p3.getI() - p4.getI() + 0.);
+        b1 = p1.getJ() - a1 * p1.getI();
+        b2 = p3.getJ() - a2 * p3.getI();
+
+        // Прямые параллельны (возможно совпадение прямых).
+        if (a1 == a2)
+            return no;
+
+        x0 = (b2 - b1) / (a1 - a2);
+
+        // Точка x0 находится на пересечении проекций внутренностей отрезков на ось абсцисс.
+        if (x0 > Math.max(p1.getI(), p3.getI()) && x0 < Math.min(p2.getI(), p4.getI()))
+            return new Pixel(x0, a1 * x0 + b1);
+        else
+            return no;
+    }
+}
