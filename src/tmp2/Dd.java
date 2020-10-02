bc7797e916b18a71c602faa0302f0a0dfb9e25a3
Ruslan Shikhov <shikhovruslan@yandex.ru>
2020-10-02 17:48:53 +0700

    02.10.20 - 1 W

---
+++ src/tmp2/Dd.java
@@ -0,0 +1,17 @@
+package tmp2;
+
+class GenericFactory<T> {
+    Class theClass = null;
+
+    public GenericFactory(Class theClass) {
+        this.theClass = theClass;
+    }
+
+    public T createInstance()
+            throws IllegalAccessException, InstantiationException {
+        return (T) this.theClass.newInstance();
+    }
+}
+
+public class Dd {
+}
